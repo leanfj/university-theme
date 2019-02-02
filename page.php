65c9c07a28fc1bcbc6ea9f53bdda9591ca0a4602
leanfj <leanfj@hotmail.com>
2019-02-02 21:01:53 -0200

    Modificações em page

--- page.php
+++ page.php
@@ -15,17 +15,45 @@
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
     
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div> -->
+    
+    <?php
+      $theParent = wp_get_post_parent_id(get_the_ID()); 
+      if ($theParent) { ?>
+      
+        <div class="metabox metabox--position-up metabox--with-home-link">
+          <p><a class="metabox__blog-home-link" href="<?php echo get_permalink( $theParent ) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent ); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+        </div>
+        
+    <?php } ?>
+    <?php 
+      $testPage = get_pages(array(
+        'child_of' => get_the_ID(  )
+      ));
+    
+      if ($theParent or $testPage) { ?> 
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink( $theParent )?>"><?php echo get_the_title($theParent); ?></a></h2>
+        <ul class="min-list">
+          <?php 
+            if ($theParent) {
+              # code...
+              $findChildOf = $theParent;
+            } else {
+              $findChildOf = get_the_ID(  );
+            } 
+          
+            wp_list_pages( array(
+              'title_li' => NULL,
+              'child_of' => $findChildOf,
+              'sort_column' => 'menu_order'
+            ) )
+          
+          ?>
+        </ul>
+      </div>
+    <?php } ?>
+
 
     <div class="generic-content">
       <?php the_content( ); ?>
