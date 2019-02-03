6724c1ae744cee0456f752d11e8969a3e6c23fcd
leanfj <leanfj@hotmail.com>
2019-02-03 15:14:58 -0200

    Introdução de menu customizado e via wordpress

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <header class="site-header">
     <div class="container">
       <h1 class="school-logo-text float-left"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
@@ -15,12 +15,16 @@
       <div class="site-header__menu group">
         <nav class="main-navigation">
           <ul>
-            <li><a href="<?php echo site_url( 'index.php/about-us' );?>">About Us</a></li>
+            <li <?php 
+            if (is_page( 'about-us' ) or wp_get_post_parent_id( 0 ) == 21 )echo 'class="current-menu-item"'; ?>><a href="<?php echo site_url( 'index.php/about-us' );?>">About Us</a></li>
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
             <li><a href="#">Blog</a></li>
           </ul>
+          <!-- <?php wp_nav_menu(array(
+            'theme_location' => 'headerMenuLocation'
+          )); ?> -->
         </nav>
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
