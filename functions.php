c3e49b8dc9053bf23fb5f8be171d163dd6cd8303
leanfj <leanfj@hotmail.com>
2019-02-15 07:23:42 -0200

    Add new post type

--- functions.php
+++ functions.php
@@ -16,4 +16,18 @@
     }
 
     add_action( 'after_setup_theme', 'lfwi_university_features' );
-?>
+
+    // function lfwi_university_post_types() {
+    // register_post_type( 'event', array(
+    //     'public' => true,
+    //     'menu_icon' => 'dashicons-calendar-alt',
+    //     'labels' => array(
+    //         'name' => 'Events',
+    //         'add_new_item' => 'Add New Events',
+    //         'edit_item' => 'Edit Event',
+    //         'all_items' => 'All Events',
+    //         'singular_name' => 'Event'
+    //     )
+    // ) );
+    // }
+    // add_action( 'init', 'lfwi_university_post_types');
--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
-exports.themeLocation = './';
-exports.urlToPreview = 'http://localhost';
+exports.themeLocation = "./";
+exports.urlToPreview = "http://localhost/curso_udemy/index.php";
 
 // If you're using Local by Flywheel you will
 // want your settings to be similar to the examples below:
